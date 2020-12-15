f9b6e1eebdea41aea23e490c367c0e26ab6948d2
AlbertD31 <75625289+AlbertD31@users.noreply.github.com>
2020-12-15 14:47:59 +0000

    Add files via upload

---
+++ PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController2D controller;
+    float horizontalMove = 10f;
+    public float walkSpeed = 40f;
+    bool jump = false;
+    private Animator animator; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var walaking = false;
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        if (Mathf.Abs(horizontalMove) > 0)
+        {
+            walaking = true;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+        animator.SetBool("isWalking", walaking);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
---
+++ cameraFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowPlayer : MonoBehaviour
+{
+
+    GameObject  player;
+    Vector3 playerPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (player != null && player.transform.position.y > -2f)
+        {
+            playerPosition = new Vector3 (player.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+
+            gameObject.transform.position = playerPosition;
+        }
+
+
+    }
+}
