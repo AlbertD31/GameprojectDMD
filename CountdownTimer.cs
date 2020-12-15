de1412ec67dec9441cb1fe4c99f6e778c9277162
jennissz <75625890+jennissz@users.noreply.github.com>
2020-12-16 00:33:50 +0700

    Add files via upload

---
+++ CountdownTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour 
+{
+	float currentTime = 0f;
+	float startingTime = 120f;
+
+	[SerializeField] Text countdownText;
+
+	void Start()
+
+	{
+		currentTime = startingTime;
+
+	}
+
+	void Update()
+	{
+		currentTime -= 1 * Time.deltaTime;
+		countdownText.text = currentTime.ToString ("0");
+		
+		if (currentTime <= 0)
+		{
+			currentTime = 0;
+		}
+	}
+}
