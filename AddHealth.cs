06a877eb59918218fc83a26c6ea9e41588b7f221
sophiejane989 <62431279+sophiejane989@users.noreply.github.com>
2020-12-21 15:43:23 +0000

    Adding health
    currently isnt work can anyone help me? ive created a new add health script which is attached tot he sheep and added a new public void of AddHealth to the players health manager.

---
+++ AddHealth.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddHealth : MonoBehaviour
+{
+
+    PlayerHealth playerHealth;
+    bool healthAdded;   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        
+
+        healthAdded = true;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && healthAdded)
+        {
+            healthAdded = false;
+            Debug.Log("Health added!");
+
+            
+
+            if (playerHealth != null)
+            {
+                playerHealth.AddHealth(50);
+
+            }
+            DestroyPlayer();
+        }
+    }
+
+    void DestroyPlayer()
+    {
+
+        gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        gameObject.GetComponentInChildren<Collider2D>().enabled = false;
+
+    }
+
+}
---
+++ PlayerHealth.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    public HealthBar healthBar;
+    Slider healthSlider;
+    
+    enemyController enemyController;
+    CanvasGroup damageOverlay;
+    bool PlayerCanDie;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        if (healthBar != null)
+        {
+        healthBar.SetMaxHealth(maxHealth);
+
+        }
+       
+        if (enemyController != null)
+        {
+            enemyController = GameObject.FindGameObjectWithTag
+                  ("enemy").GetComponent<enemyController>();
+        }
+        damageOverlay = GameObject.Find("DamageOverlay").GetComponent<CanvasGroup>();
+        PlayerCanDie = true;
+
+    }
+    public void AddHealth(int amout)
+    {
+        currentHealth += amout;
+
+        if (currentHealth > 100)
+        {
+            currentHealth = 100;
+        }
+       
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
+
+    }
+
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void Update()
+    {
+        //if (Input.GetButtonDown("HealthTest"))
+        //{
+        //    TakeDamage(20);
+        //}
+        if (currentHealth <= 0 && PlayerCanDie)
+        {
+            playerDies();
+            PlayerCanDie = false;
+        }
+    }
+    public void playerDies()
+    {              
+        StartCoroutine(damageFade());
+
+    }
+    IEnumerator damageFade()
+    {
+
+        damageOverlay.alpha = 1f;
+        yield return new WaitForSeconds(0.3f);
+        damageOverlay.alpha = 0f;
+    }
+
+
+
+}
